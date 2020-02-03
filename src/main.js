2160da6d513ddd75272cd7a7f7a3e21c760cac76
Student <example@example.com>
2020-02-03 15:03:55 -0800

    refactor display stats function

--- src/main.js
+++ src/main.js
@@ -2,15 +2,19 @@ import './bootstrap.css';
 import './styles.css';
 import { Entry } from './entry';
 
+function displayStats(entry){
+  $("#wordCount").text("Word Count: " + entry.numberOfWords());
+  $("#vowelCount").text("Vowel Count: " + entry.numberOfVowels());
+  $("#consCount").text("Consonant Count: " + entry.numberOfConsonants());
+  $("#teaser").text("Preview: " + entry.heading + " - " + entry.getTeaser() + "...............");
+}
+
 $(document).ready(function() {
   $("#new-journal-entry-form").submit(function(event){
     event.preventDefault();
     console.log("submitted");
     var entry = new Entry ($("#entry-heading").val(), $("#entry-body").val());
     console.log(entry);
-     $("#wordCount").text("Word Count: " + entry.numberOfWords());
-     $("#vowelCount").text("Vowel Count: " + entry.numberOfVowels());
-     $("#consCount").text("Consonant Count: " + entry.numberOfConsonants());
-     $("#teaser").text("Preview: " + entry.heading + " - " + entry.getTeaser() + "...............");
+    displayStats(entry);
   });
 });
